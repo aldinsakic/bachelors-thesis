32bc840fb01e391e676e8a506a9ca271a9cee182
Aldin Sakic <122877826+aldinsakic@users.noreply.github.com>
2024-04-19 01:47:24 +0200

    Update js.js

--- d3/js.js
+++ d3/js.js
@@ -1,9 +1,127 @@
-// import {legend} from "@d3/color-legend"
+function filter(p) {
+  // // import {legend} from "@d3/color-legend"
+
+// // set the dimensions and margins of the graph
+// const margin = {top: 30, right: 30, bottom: 30, left: 30},
+//   width = 600 - margin.left - margin.right,
+//   height = 600 - margin.top - margin.bottom;
+
+// // append the svg object to the body of the page
+// const svg = d3.select("#my_dataviz")
+// .append("svg")
+//   .attr("width", width + margin.left + margin.right)
+//   .attr("height", height + margin.top + margin.bottom)
+// .append("g")
+//   .attr("transform", `translate(${margin.left},${margin.top})`);
+
+//   // Labels of row and columns
+//   const myGroups = ["2023", "2022", "2021", "2020", "2019", "2017", "2015", "2013"];
+//   const myVars = ["BE","BG","CZ","DK","DE","EE","IE","EL","ES","FR","HR","IT","CY","LV","LT","LU","HU","MT","NL","AT","PL","PT","RO","SI","SK","FI","SE"];
+  
+  
+//   // Build X scales and axis:
+//   const x = d3.scaleBand()
+//   .range([width, 0])
+//   .domain(myGroups)
+//   //.padding(0.01);
+//   svg.append("g")
+//   .attr("transform", `translate(0, ${height})`)
+//   .call(d3.axisBottom(x))
+  
+//   // Build y scales and axis:
+//   const y = d3.scaleBand()
+//   .range([height, 0])
+//   .domain(myVars)
+//   //.padding(0.01);
+//   svg.append("g")
+//   .call(d3.axisLeft(y));
+
+
+// //svg.selectAll(".secondrow").data(data).enter().append("circle").attr("cx", function(d,i){return 30 + i*60}).attr("cy", 250).attr("r", 19).attr("fill", function(d){return myColor(d) })
+
+// // const tooltip = d3.select("#my_dataviz")
+// //   .append("div")
+// //   .style("opacity", 0)
+// //   .attr("class", "tooltip")
+// //   .style("background-color", "white")
+// //   .style("border", "solid")
+// //   .style("border-width", "2px")
+// //   .style("border-radius", "5px")
+// //   .style("padding", "5px")
+
+// const mouseleave = function(event,d) {
+//   value
+//     .style("opacity", 0)
+//   // d3.select(this)
+//   //   .style("stroke", "none")
+//   //   .style("opacity", 0.8)
+// }
+
+// const value = d3.select("#my_dataviz")
+//   .append("p")
+
+// const mouseover = function(event,d) {
+//   // let thisX = event.target.attributes[0].nodeValue;
+//   // let thisY = event.target.attributes[1].nodeValue;
+//   value
+//     //.style("margin", Math.round(thisX)+"px")
+//     .style("opacity", 1)
+//     //.append("text")
+//     .html(d.value)
+//   //   .style("opacity", 1)
+//   // d3.select(this)
+//   //   .style("stroke", "black")
+//   //   .style("opacity", 1)
+//   //console.log(d.value);
+// }
+// //Read the data
+// d3.csv("https://raw.githubusercontent.com/aldinsakic/bachelors-thesis/main/standardDateGEI.csv").then( function(data) {
+  
+
+//   // const mousemove = function(event,d) {
+//   //   value
+//   //     //.html(d.value)
+//   //     //.style("left", (event.x)/2 + "px")
+//   //     //.style("top", (event.y)/2 + "px")
+//   //   console.log(d);
+      
+//   // }
+  
+//   // figure out the max and min values, to be used in the color scale
+//   let tmpArray = [];
+//   for (var i=0; i<data.length; i++) {
+//     row = data[i];
+//     tmpArray.push(row['value'])
+//   }
+//   const maxValue = Math.max.apply(null, tmpArray);
+//   const minValue = Math.min.apply(null, tmpArray);
+//   // Build color scale
+//   const myColor = d3.scaleSequential().domain([minValue,maxValue]).interpolator(d3.interpolateViridis);
+  
+//   //svg.selectAll()
+//   // this is so borked, fix time 
+//   svg.selectAll()
+//   .data(data, function(d) {return d})
+//   .enter()
+//   .append("rect")
+//     .attr("x", function(d) { return x(d.group)})
+//     .attr("y", function(d) { return y(d.variable)})
+//     .attr("width", x.bandwidth() )
+//     .attr("height", y.bandwidth() )
+//     .style("fill", function(d) { return myColor(d.value)} )
+//     //.style("stroke-width", 1)
+//     //.style("stroke", "none")
+//     //.style("opacity", 1)
+//     .on("mouseover", mouseover)
+//     //.on("mousemove", mousemove)
+//     .on("mouseleave", mouseleave)
+//   // .append("text").attr("x", 500).attr("y", 599).text("variable A").style("font-size", "15px").attr("alignment-baseline","middle")
+// })
 
 // set the dimensions and margins of the graph
-const margin = {top: 30, right: 30, bottom: 30, left: 30},
-  width = 600 - margin.left - margin.right,
-  height = 600 - margin.top - margin.bottom;
+const margin = {top: 80, right: 25, bottom: 30, left: 50},
+  width = 800 - margin.left - margin.right,
+  height = 800 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 const svg = d3.select("#my_dataviz")
@@ -11,82 +129,16 @@ const svg = d3.select("#my_dataviz")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
 .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
-
-  // Labels of row and columns
-  const myGroups = ["2013", "2015", "2017", "2019", "2020", "2021", "2022", "2023"];
-  const myVars = ["BE","BG","CZ","DK","DE","EE","IE","EL","ES","FR","HR","IT","CY","LV","LT","LU","HU","MT","NL","AT","PL","PT","RO","SI","SK","FI","SE"];
-  
-  
-  // Build X scales and axis:
-  const x = d3.scaleBand()
-  .range([width, 0])
-  .domain(myGroups)
-  //.padding(0.01);
-  svg.append("g")
-  .attr("transform", `translate(0, ${height})`)
-  .call(d3.axisBottom(x))
-  
-// Build X scales and axis:
-const y = d3.scaleBand()
-  .range([height, 0])
-  .domain(myVars)
-  //.padding(0.01);
-svg.append("g")
-  .call(d3.axisLeft(y));
-
-
-//svg.selectAll(".secondrow").data(data).enter().append("circle").attr("cx", function(d,i){return 30 + i*60}).attr("cy", 250).attr("r", 19).attr("fill", function(d){return myColor(d) })
-
-// const tooltip = d3.select("#my_dataviz")
-//   .append("div")
-//   .style("opacity", 0)
-//   .attr("class", "tooltip")
-//   .style("background-color", "white")
-//   .style("border", "solid")
-//   .style("border-width", "2px")
-//   .style("border-radius", "5px")
-//   .style("padding", "5px")
-
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 //Read the data
-d3.csv("https://raw.githubusercontent.com/aldinsakic/bachelors-thesis/main/standardDateGEI.csv").then( function(data) {
-  
+d3.csv("https://raw.githubusercontent.com/aldinsakic/bachelors-thesis/main/standardDateGEI.csv").then(function(data) {
 
-  const value = d3.select("#my_dataviz")
-    .append("p")
+  // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
+  const myGroups = Array.from(new Set(data.map(d => d.group)))
+  const myVars = Array.from(new Set(data.map(d => d.variable)))
 
-  const mouseover = function(event,d) {
-    // let thisX = event.target.attributes[0].nodeValue;
-    // let thisY = event.target.attributes[1].nodeValue;
-    value
-      //.style("margin", Math.round(thisX)+"px")
-      .style("opacity", 1)
-      //.append("text")
-      .html(d.value)
-    //   .style("opacity", 1)
-    // d3.select(this)
-    //   .style("stroke", "black")
-    //   .style("opacity", 1)
-    //console.log(d.value);
-  }
-  // const mousemove = function(event,d) {
-  //   value
-  //     //.html(d.value)
-  //     //.style("left", (event.x)/2 + "px")
-  //     //.style("top", (event.y)/2 + "px")
-  //   console.log(d);
-      
-  // }
-  const mouseleave = function(event,d) {
-    value
-      .style("opacity", 0)
-    // d3.select(this)
-    //   .style("stroke", "none")
-    //   .style("opacity", 0.8)
-  }
-  
-  // figure out the max and min values, to be used in the color scale
+    // figure out the max and min values, to be used in the color scale
   let tmpArray = [];
   for (var i=0; i<data.length; i++) {
     row = data[i];
@@ -94,26 +146,105 @@ d3.csv("https://raw.githubusercontent.com/aldinsakic/bachelors-thesis/main/stand
   }
   const maxValue = Math.max.apply(null, tmpArray);
   const minValue = Math.min.apply(null, tmpArray);
+  console.log(maxValue, minValue);
   // Build color scale
-  const myColor = d3.scaleSequential().domain([minValue,maxValue]).interpolator(d3.interpolateViridis);
-  
-  //svg.selectAll()
-  // this is so borked, fix time 
+  //const myColor = d3.scaleSequential().domain([minValue,maxValue]).interpolator(d3.interpolateViridis);
+
+  // Build X scales and axis:
+  const x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(myGroups)
+    // .padding(0.05);
+  svg.append("g")
+    .style("font-size", 15)
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x).tickSize(0))
+    .select(".domain").remove()
+
+  // Build Y scales and axis:
+  const y = d3.scaleBand()
+    .range([ height, 0 ])
+    .domain(myVars)
+    // .padding(0.05);
+  svg.append("g")
+    .style("font-size", 15)
+    .call(d3.axisLeft(y).tickSize(0))
+    .select(".domain").remove()
+
+  // Build color scale
+  const myColor = d3.scaleSequential()
+    .interpolator(d3.interpolateViridis)
+    // hard coded min and max values, change later
+    .domain([48.6, 83.9])
+
+
+  // create a tooltip
+  const tooltip = d3.select("#my_dataviz")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px")
+
+  // Three function that change the tooltip when user hover / move / leave a cell
+  const mouseover = function(event,d) {
+    tooltip
+      .style("opacity", 1)
+    d3.select(this)
+      // .style("stroke", "black")
+      .style("opacity", 1)
+  }
+  const mousemove = function(event,d) {
+    tooltip
+      .html("Value: " + d.value + ", Date: " + d.variable + ", Country: " + d.group)
+      .style("left", (event.x)/2 + "px")
+      .style("top", (event.y)/2 + "px")
+  }
+  const mouseleave = function(event,d) {
+    tooltip
+      .style("opacity", 0)
+    d3.select(this)
+      // .style("stroke", "none")
+      // .style("opacity", 0.8)
+  }
+
+  // add the squares
   svg.selectAll()
-  .data(data, function(d) {return d.group+':'+d.variable;})
-  .enter()
-  // .scales({myColor})
-  .append("rect")
-    .attr("x", function(d) { return x(d.group)+1 })
-    .attr("y", function(d) { return y(d.variable)+1 })
-    .attr("width", x.bandwidth() )
-    .attr("height", y.bandwidth() )
-    .style("fill", function(d) { return myColor(d.value)} )
-    //.style("stroke-width", 1)
-    //.style("stroke", "none")
-    //.style("opacity", 1)
+    .data(data, function(d) {return d.group+':'+d.variable;})
+    .join("rect")
+      .attr("x", function(d) { return x(d.group) })
+      .attr("y", function(d) { return y(d.variable) })
+      // .attr("rx", 4)
+      // .attr("ry", 4)
+      .attr("width", x.bandwidth() )
+      .attr("height", y.bandwidth() )
+      .style("fill", function(d) { return myColor(d.value)} )
+      .style("stroke-width", 4)
+      .style("stroke", "none")
+      // .style("opacity", 0.8)
     .on("mouseover", mouseover)
-    //.on("mousemove", mousemove)
+    .on("mousemove", mousemove)
     .on("mouseleave", mouseleave)
-  // .append("text").attr("x", 500).attr("y", 599).text("variable A").style("font-size", "15px").attr("alignment-baseline","middle")
 })
+
+// Add title to graph
+svg.append("text")
+        .attr("x", 0)
+        .attr("y", -50)
+        .attr("text-anchor", "left")
+        .style("font-size", "14px")
+        .text("GEI in EU across all avaliable years, interactive heatmap");
+
+// Add subtitle to graph
+// svg.append("text")
+        // .attr("x", 0)
+        // .attr("y", -20)
+        // .attr("text-anchor", "left")
+        // .style("font-size", "14px")
+        // .style("fill", "grey")
+        // .style("max-width", 400)
+        // .text("A short description of the take-away message of this chart.");
+}
