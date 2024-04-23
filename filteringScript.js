39d40f6c2029ba5f2f6d8e31334a9ba3e51b66a1
aldinsakic <aldinsakic@duck.com>
2024-04-24 01:22:19 +0200

    added userscript

---
+++ filteringScript.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        New script plotly-server.local
+// @namespace   Violentmonkey Scripts
+// @match       http://plotly-server.local/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 4/23/2024, 7:27:07 PM
+// ==/UserScript==
+window.addEventListener("load", (event) => {
+    onLoad(['BE']);
+    onLoad(['DE']);
+    onLoad(['BE', 'DK']);
+    onLoad(['ES', 'CZ', 'EL','ES','FR','HR','IT','CY','LV','LT','LU','EE']);
+    onLoad(['FI', 'SE', 'DK']);
+  });
