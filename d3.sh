0e717ed813d4e9093c36916e2171e19aa8427a6b
aldinsakic <aldinsakic@duck.com>
2024-04-24 01:41:21 +0200

    updating scripts

--- d3.sh
+++ d3.sh
@@ -3,4 +3,5 @@ for i in $(seq 1 200)
 do
     firefox -no-remote d3-server.local &
     sleep 20
+    pkill -f firefox
 done
--- plotly.sh
+++ plotly.sh
@@ -3,4 +3,5 @@ for i in $(seq 1 200)
 do
     firefox -no-remote plotly-server.local &
     sleep 20
+    pkill -f firefox
 done
