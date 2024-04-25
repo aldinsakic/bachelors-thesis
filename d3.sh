7338ad58485acb451bc771459126cc452087e3a5
aldinsakic <aldinsakic@duck.com>
2024-04-26 01:12:57 +0200

    vm and .local addresses dont get along it seems

--- d3.sh
+++ d3.sh
@@ -1,7 +1,7 @@
 # 100 for loop
 for i in $(seq 1 100)
 do
-    firefox d3-server.local &
+    firefox 192.168.1.112 &
     sleep 30
     echo $i
 done
--- plotly.sh
+++ plotly.sh
@@ -1,7 +1,7 @@
 # 100 for loop
 for i in $(seq 1 100)
 do
-    firefox plotly-server.local &
+    firefox 192.168.1.229 &
     sleep 30
     echo $i
 done
