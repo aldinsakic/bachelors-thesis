a176804ea0b42af6dcab244fc1c723aac976ac99
aldinsakic <aldinsakic@duck.com>
2024-04-26 00:09:07 +0200

    removed no-remote

--- d3.sh
+++ d3.sh
@@ -1,7 +1,7 @@
 # 100 for loop
 for i in $(seq 1 100)
 do
-    firefox -no-remote d3-server.local &
+    firefox d3-server.local &
     sleep 30
     pkill -f firefox
     echo $i
--- plotly.sh
+++ plotly.sh
@@ -1,7 +1,7 @@
 # 100 for loop
 for i in $(seq 1 100)
 do
-    firefox -no-remote plotly-server.local &
+    firefox plotly-server.local &
     sleep 30
     pkill -f firefox
     echo $i
