72e8a877c5f04f2accc051c08ea0d95cb07523b5
aldinsakic <aldinsakic@duck.com>
2024-05-15 18:46:14 +0200

    created all the deltas for data

--- experimentData/run2-1000/dataScripts/calculateDiff.py
+++ experimentData/run2-1000/dataScripts/calculateDiff.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
-start = "../d3/filterStartTimeD3.txt"
+# start = "../d3/filterStartTimeD3.txt"
+start = input("start: ")
 start_df = pd.read_csv(start, header=None, names=['epoch'])
-end = "../d3/filterEndTimeD3.txt"
+# end = "../d3/filterEndTimeD3.txt"
+end = input("end: ")
 end_df = pd.read_csv(end, header=None, names=['epoch'])
 
 delta = end_df['epoch'] - start_df['epoch']
